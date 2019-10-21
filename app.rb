e3f53dbf27664b375988dfa14eff6a6cebad137c
Doug Kintop <dkintop@hotmail.com>
2019-10-21 20:02:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
   # Write your code here!
 
+
+  get '/reversename/:name' do 
+    
+    @name = params[:name]
+  binding.pry 
+  end 
+  
 end
