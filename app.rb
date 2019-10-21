5fbaddc6180c09ea4b69ec1678768f010b9a8fca
Doug Kintop <dkintop@hotmail.com>
2019-10-21 20:06:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,10 +5,13 @@ class App < Sinatra::Base
 
 
   get '/reversename/:name' do 
-    
     @name = params[:name]
     @name.reverse 
-    
+  end 
+  
+  get '/square/:number' do 
+    @number = params[:number].to_i
+    square = @number * @number.to_s
   end 
   
 end
